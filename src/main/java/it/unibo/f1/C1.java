2c6e847dc2454bc0e748d79ffe63e7ffa5358884
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-20 11:27:06 +0200

    feat: c1 class

---
+++ src/main/java/it/unibo/f1/C1.java
@@ -0,0 +1,16 @@
+package it.unibo.f1;
+
+/**
+ * Feature 1
+ *
+ * @author andreacecchini
+ */
+public class C1 {
+    public String m() {
+        return "Feature 1";
+    }
+
+    public static void main(final String... args) {
+        System.out.println(new C1().m());
+    }
+}
