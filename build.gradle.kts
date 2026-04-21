260716cd54c5dc2a7184c4f81d89fb2a28f522ea
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-21 16:00:20 +0200

    feat: added junit 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,11 @@ repositories {
 
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.7.4")
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
